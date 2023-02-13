e5293d2280fb13ced9e3d9d4f6026c58dc06a4fb
ee0122ng <ee22011993hang@gmail.com>
2023-02-13 20:54:09 +0800

    add random image generator via GetMapping, RequestBody with MultiValueMap, and ModelAttribute

--- selfRevision-ws02/src/main/java/iss/ssf/selfRevisionws02/controller/NumberController.java
+++ selfRevision-ws02/src/main/java/iss/ssf/selfRevisionws02/controller/NumberController.java
@@ -2,7 +2,6 @@ package iss.ssf.selfRevisionws02.controller;
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
@@ -11,8 +10,9 @@ import java.util.logging.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import iss.ssf.selfRevisionws02.model.Input;
 
@@ -33,6 +33,8 @@ public class NumberController {
         return "number";
     }
 
+    // Via GetMapping
+    /*
     @GetMapping(path="/totalImage")
     public String getImage(@RequestParam("num") Integer value, Model model) {
 
@@ -54,5 +56,60 @@ public class NumberController {
 
         return "image";
     }
+    */
+
+    // Via PostMapping with MutliValueMap
+    /*
+         @PostMapping(path="/totalImage")
+    public String getImage(@RequestBody MultiValueMap<String, String> input, Model model) {
+
+        Integer value = Integer.valueOf(input.getFirst("num"));
+
+        List<Integer> imageList = new ArrayList<Integer>();
+        Random rand = new SecureRandom();
+
+        for(int i=0; i < value; i++) {
+            imageList.add(rand.nextInt(31));
+        }
+
+        // use logger to display out
+        String clientInput = "Client input number: " + value;
+        String image = "Random image list: " + imageList;
+
+        logger.log(Level.INFO, clientInput);
+        logger.log(Level.INFO, image);
+
+        model.addAttribute("imageList", imageList);
+
+        return "image";
+    }
+     */
+
+     // Via PostMapping with ModelAttribute
+    @PostMapping(path="/totalImage")
+    public String getImage(@ModelAttribute("input") Input input, Model model) {
+
+        Integer value = input.getNum();
+
+        List<Integer> imageList = new ArrayList<Integer>();
+        Random rand = new SecureRandom();
+
+        for(int i=0; i < value; i++) {
+            imageList.add(rand.nextInt(31));
+        }
+
+        // use logger to display out
+        String clientInput = "Client input number: " + value;
+        String image = "Random image list: " + imageList;
+
+        logger.log(Level.INFO, clientInput);
+        logger.log(Level.INFO, image);
+
+        model.addAttribute("imageList", imageList);
+
+        return "image";
+    }
+
+
     
 }
